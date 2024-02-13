58f7c14d4d736015027700763a809ec33770aaf2
dmitriy-stakin <mailofme@mail.ru>
2024-02-13 19:39:54 +0700

    1.5 доп1

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,3 @@
 function truncate(str, maxlength) {
-  return (str.length > maxlength) ? str.slice(0,maxlength).replace(/.$/,"...") : str
+  return (str.length > maxlength) ? str.slice(0,maxlength).replace(/.$/,"…") : str
 }
