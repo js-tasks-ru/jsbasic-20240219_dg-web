1f1c04946734bdbc13d6f9b0d458fab861bd84b9
dmitriy-stakin <mailofme@mail.ru>
2024-02-16 14:59:50 +0700

    question

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,9 @@
 function isEmpty(obj) {
   return (Object.keys(obj).length > 0) ? false : true
 }
+
+// function isEmpty(obj) {
+//   for (const key in obj) {
+//     return (Object.hasOwn(obj, key)) ? false : true
+//   }
+// }
