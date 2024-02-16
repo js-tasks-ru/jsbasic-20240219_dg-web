71fd5806e8bb8b37e1771f2674dd0b16243f7f61
dmitriy-stakin <mailofme@mail.ru>
2024-02-16 15:20:08 +0700

    2.3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,17 @@
 let calculator = {
-  // ваш код
+  first : 0,
+  second : 0,
+
+  read(a,b) {
+    this.first = a
+    this.second = b
+  },
+  sum() {
+    return this.first + this.second
+  },
+  mul() {
+    return this.first * this.second
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
