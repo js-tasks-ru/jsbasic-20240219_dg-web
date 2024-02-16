ec65936933d3790e1ed55a232938b476fa9e0012
dmitriy-stakin <95010440+dmitriy-stakin@users.noreply.github.com>
2024-02-16 15:21:43 +0700

    Update index.js
    undo

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,9 +1,3 @@
 function isEmpty(obj) {
   return (Object.keys(obj).length > 0) ? false : true
 }
-
-// function isEmpty(obj) {
-//   for (const key in obj) {
-//     return (Object.hasOwn(obj, key)) ? false : true
-//   }
-// }
