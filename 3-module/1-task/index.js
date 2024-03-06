4323bd165111520bee4286148094ef05ea3206ba
dmitriy-stakin <mailofme@mail.ru>
2024-03-06 16:41:36 +0700

    3.1, 3.2 fix

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,6 +1,6 @@
 function namify(users) {
   if(users) {
-    users.forEach((user, i) => {
+    users.map((user, i)=>{
       for(key in user) {
         (key == 'name') ? users.splice(i, 1, user[key]) : null
       }
--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,7 +1,3 @@
 function filterRange(arr, a, b) {
-    let res = []
-    arr.forEach(el => {
-      (a <= el && el <= b) ? res.push(el) : null
-    })
-    return res
+  return arr.filter(item => a <= item && item <= b)
 }
